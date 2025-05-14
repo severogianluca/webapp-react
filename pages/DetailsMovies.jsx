79d75c476fd2c99c73b434a751cee00d0b59e09a
severogianluca <gianlucasevero300@gmail.com>
2025-05-14 05:16:36 +0200

    fix btn

--- pages/DetailsMovies.jsx
+++ pages/DetailsMovies.jsx
@@ -36,7 +36,7 @@ function DetailsMovies() {
                                             <p className="card-text">Genere: {movie.genre}</p>
                                             <p className="card-text">Anno: {movie.release_year}</p>
                                             <p className="card-text">Descrizione: {movie.abstract}</p>
-                                            <Link to={'/'} className="btn btn-primary mt-4">Indietro</Link>
+                                            <Link to={'/'} className="btn btn-primary mt-3">Indietro</Link>
                                         </div>
 
                                         <p className="card-text">Media voto: {movie.average_vote}</p>
