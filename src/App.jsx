21931c7ca4b49a7f81c1e63cd3658898536e35b1
severogianluca <gianlucasevero300@gmail.com>
2025-05-15 17:34:05 +0200

    Feat: create context

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,29 @@ import './App.css'
 import HomeListMovies from "../pages/HomeListMovies";
 import DetailsMovies from "../pages/DetailsMovies";
 import ChiSiamo from "../pages/ChiSiamo";
+import GlobalContext from "../context/globalContext";
+import { useState } from "react";
 
 function App() {
+
+  const[isLoading, setIsLoading] = useState(false)
 
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
+      <GlobalContext.Provider value={{isLoading, setIsLoading}}>
+        <BrowserRouter>
+          <Routes>
 
-          <Route  element={<DefaultLayout/>}>
-            <Route path="/" element={<HomeListMovies/>} />
-            <Route path="/ChiSiamo" element={<ChiSiamo/>} />
-            <Route path="/:id" element={<DetailsMovies/>} />
-          </Route>
+            <Route element={<DefaultLayout />}>
+              <Route path="/" element={<HomeListMovies />} />
+              <Route path="/ChiSiamo" element={<ChiSiamo />} />
+              <Route path="/:id" element={<DetailsMovies />} />
+            </Route>
 
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </GlobalContext.Provider>
     </>
   )
 }
